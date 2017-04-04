54a5ea41ece0f2d1ef734c29936982843e072601
Asato Wakisaka <asato.wakisaka@gmail.com>
2017-04-04 10:48:46 +0900

    code styling

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-if [[ $apikey ]]; then
+if [ "$apikey" != "" ]; then
     sed -i -e "s|# apikey = \"\"|apikey = \"${apikey}\"|" /etc/mackerel-agent/mackerel-agent.conf
 fi
 
-if [[ $include ]]; then
+if [ "$include" != "" ]; then
     sed -i -e "s|# Configuration for Custom Metrics Plugins|include = \"${include}\"|" /etc/mackerel-agent/mackerel-agent.conf
 fi
 
