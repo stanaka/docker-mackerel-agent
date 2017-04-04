29e1a6fe836d2691ee06dbd689d60be158af5538
Songmu <y.songmu@gmail.com>
2017-04-04 11:24:44 +0900

    adjust startup.sh

--- startup.sh
+++ startup.sh
@@ -1,25 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 prog=/usr/bin/mackerel-agent
+conf=/etc/mackerel-agent/mackerel-agent.conf
 
 if [ "$apikey" != "" ]; then
-    $prog init -apikey=${apikey}
+    $prog init -apikey="$apikey"
 fi
 
 if [ "$include" != "" ]; then
-    sed -i -e "s|# Configuration for Custom Metrics Plugins|include = \"${include}\"|" /etc/mackerel-agent/mackerel-agent.conf
+    sed -i -e "s|# Configuration for Custom Metrics Plugins|include = \"${include}\"|" $conf
 fi
 
-if [ "$enable_docker_plugin" != "" ] && [ "$enable_docker_plugin" != "0"] && ! grep "^\[plugin\.metrics\.docker\]" /etc/mackerel-agent/mackerel-agent.conf; then
-    echo [plugin.metrics.docker] >> /etc/mackerel-agent/mackerel-agent.conf
-    echo command = \"/usr/bin/mackerel-plugin-docker -method API -name-format name\" >> /etc/mackerel-agent/mackerel-agent.conf
+if [ "$enable_docker_plugin" != "" ] && [ "$enable_docker_plugin" != "0" ] && ! grep '^[plugin.metrics.docker]' $conf; then
+    cat >> $conf << "EOF"
+[plugin.metrics.docker]
+command = "/usr/bin/mackerel-plugin-docker -method API -name-format name"
+EOF
 fi
 
 sig_trap() {
-    func="$1"
-    shift $@
-    for sig in $@; do
+    func="$1"; shift
+    for sig in "$@"; do
         trap "$func $sig" "$sig"
     done
 }
@@ -29,13 +31,13 @@ cleanup() {
     if [ "$auto_retirement" != "" ] && [ "$auto_retirement" != "0" ]; then
         $prog retire -force $opts
     fi
-    kill -$sig $PID
+    kill -"$sig" "$PID"
 }
 
 # Propagate signals to mackerel-agent.
 sig_trap cleanup INT TERM QUIT HUP
 
-echo /usr/bin/mackerel-agent -apikey=${apikey} $opts
+echo /usr/bin/mackerel-agent -apikey="$apikey" $opts
 /usr/bin/mackerel-agent $opts &
 PID=$!
 wait $PID
